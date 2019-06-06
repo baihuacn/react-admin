be22766aa2adceeca648cc24938599412a32761c
renjunlin <junlin.ren@qq.com>
2019-06-07 00:53:09 +0800

    feat(基础建设):调整toFixed函数

--- src/utils/math.js
+++ src/utils/math.js
@@ -5,18 +5,16 @@
  * @description 四舍五入，保留小数位
  */
 export function toFixed(value, digit) {
-  if (!value && value !== 0) {
-    return value
+  if (typeof value !== 'number') {
+    throw new Error('数据类型错误：参数value应该是number')
   }
-  try {
-    const multiple = Math.pow(10, digit)
-    const diffValue = value - 0 >= 0 ? 0.5 : -0.5
-    const processedValue = parseInt(value * multiple + diffValue, 10) / multiple
-    return processedValue.toFixed(digit)
-  } catch (err) {
-    console.error('value:', value, 'error:', err)
-    return value
+  if (typeof digit !== 'number') {
+    throw new Error('数据类型错误：参数digit应该是number')
   }
+  const multiple = Math.pow(10, digit)
+  const diffValue = value >= 0 ? 0.5 : -0.5
+  const processedValue = parseInt(value * multiple + diffValue, 10) / multiple
+  return processedValue.toFixed(digit)
 }
 /**
  * @description 判断输入的字符串是数学数字，string is number
